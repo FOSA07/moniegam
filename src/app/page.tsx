95bbee8a91ae281ff6476aca97a35884f9e2f269
FOSA07 <faletioluwaseyisam@gmail.com>
2024-09-13 12:00:03 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,116 @@
-import Image from "next/image";
+"use client"
+import { Box, Container, Typography, Stack, CircularProgress,} from "@mui/material";
+
+import React from 'react'
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    <Container maxWidth={false} disableGutters 
+      className = "bg-black h-screen w-full flex">
+        <Box sx={{ flexGrow: 1,}}>
+          <Stack direction="column" className="h-full w-full p-10 ">
+            <Container className="h-full w-full ">
+              <Stack direction="column" justifyContent="space-between" alignItems="center" className = "h-full" >
+                <Box className="flex flex-col justify-center items-center normal-case">
+                  <Typography 
+                    variant="h6"
+                    color="white"
+                    sx={{
+                      fontFamily: "var(--font-permanent-marker)",
+                      fontSize: '3rem',
+                      fontStyle: 'italic'
+                    }}>Lorem </Typography>
+                  <Typography 
+                    component="span" 
+                    variant="h6"
+                    color="white"
+                    sx={{
+                      fontFamily: "var(--font-permanent-marker)",
+                      fontSize: '3rem',
+                      fontStyle: 'italic'
+                    }}>ipsum</Typography>
+                </Box>
+                
+                <CircularProgress 
+                  size={60} 
+                  className="text-white w-10"
+                  thickness={8}
+                  sx={{
+                    '@keyframes rotate': {
+                      '0%': { transform: 'rotate(0deg)' },
+                      '100%': { transform: 'rotate(360deg)' },
+                    },
+                    animation: 'rotate 15s linear infinite',
+                  }}></CircularProgress>
+              </Stack>
+            </Container>
+            
+          </Stack>
+        </Box>
+        
+    </Container>
+  )
 }
+
+// import Image from "next/image";
+
+// export default function Home() {
+
+//   const [index, setIndex] = useState<number>(0);
+//   const [dartDojoText, setDartDojoText] = useState("");
+//   const [change, shouldChange] = useState(0);
+//   // eslint-disable-next-line @typescript-eslint/no-unused-vars
+//   const [textDone, setTextDone] = useState(false);
+
+//   useEffect(() => {
+//     const text: string = "Dart Dojo";
+
+//     const updateText = () => {
+//       if (index < text.length) {
+//         setDartDojoText(text.substring(0, index + 1));
+//         setIndex(index + 1);
+//         shouldChange(prevNum => prevNum + 10);
+//       } else {
+//         shouldChange(20)
+
+//         setTimeout(()=> {
+//           setTextDone(true);
+//         }, 1000)
+//       }
+//     };
+
+//     const timeoutId = setTimeout(updateText, 50);
+
+//     return () => clearTimeout(timeoutId);
+//   }, [index]);
+
+//   return (
+//       <Container 
+//         className = "bg-black h-screen text-white flex justify-center items-center"
+//         component='div' 
+//         disableGutters 
+//         maxWidth='xl'>
+        
+//         <Stack direction="row" gap="2" className="flex flex-col items-center">
+//           <Typography 
+//             variant="h3"
+//             sx={{
+//               fontFamily: "var(--font-permanent-marker)"
+//             }}
+//           >{dartDojoText}</Typography>
+          
+//           <Box
+//             component="span"
+//             className="bg-white"
+//             sx={{
+//               height: 7,
+//               width: change,
+//               transition: "width 0.5s",
+//             }}
+//           ></Box>
+        
+//         </Stack>
+
+//       </Container>
+//   );
+// }
